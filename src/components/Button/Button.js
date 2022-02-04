8959ef364dbff178c6c8086bdff7cc82c97edf1a
Tatiana-goit <tanchou74@gmail.com>
2022-02-05 01:42:22 +0200

    murkup registration

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,19 @@
 import s from './button.module.scss';
 
 export default function button({ title, handleClick, styleType, type }) {
-    const classStyle = () => {
-        if (styleType === "norm") { return s.norm }
-        if (styleType === "bright") { return s.bright }
-    }
-
+    // const classStyle = () => {
+    //     if (styleType === "norm") { return s.norm }
+    //     if (styleType === "bright") { return s.bright }
+    // }
+   
     return (
         <button
             type={type}
-            className={classStyle()}
-            onClick={handleClick()}>
+            className={s.button}
+            // className={[s.button,classStyle()].join(' ')}
+            // onClick={handleClick()}
+            >
+               
         {title}
         </button>
     )
