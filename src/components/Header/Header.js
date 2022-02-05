4f92c291310fdc0c8b7c609889a0a6020eed0796
Yulia Bandura <yuliabandura@gmail.com>
2022-02-05 17:11:09 +0200

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,22 @@
-import { useSelector } from 'react-redux';
-import UserMenu from '../UserMenu/UserMenu';
-import { getIsAuth } from '../../redux/auth/selectors';
-import { NavLink } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import UserMenu from "../UserMenu/UserMenu";
+import { getIsAuth } from "../../redux/auth/selectors";
+import { NavLink } from "react-router-dom";
+import styles from "./headers.module.css";
+import logo from "../../images/logo-header.png";
 
 export default function Header() {
-    const isAuth = useSelector(getIsAuth);
-    
-    return (
-        <div>
-            <header>
-                <NavLink to="/" >
-                </NavLink>
-                {isAuth ? <UserMenu /> : <></>}
-            </header>
+  const isAuth = useSelector(getIsAuth);
 
-        </div>
-    );
+  return (
+    <div>
+      <header className={styles.header}>
+        <NavLink to="/" className={styles.logo}>
+          <img src={logo} alt="" width="90" />
+        </NavLink>
+        {/* {isAuth ? <UserMenu /> : <></>} */}
+        {/* <UserMenu></UserMenu> */}
+      </header>
+    </div>
+  );
 }
--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,28 +1,35 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../redux/auth/operations'
-import { getUserName } from '../../redux/auth/selectors'
-import Button from '../Button/Button'
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/auth/operations";
+import { getUserName } from "../../redux/auth/selectors";
+import Button from "../Button/Button";
+import styles from "./userMenu.module.css";
+// import button from "../Button/Button";
 
+import logoutIcon from "../../images/icons/logout-icon.svg";
 
 export default function UserMenu() {
   const name = useSelector(getUserName);
-    const dispatch = useDispatch();
-    
-    const handleLogout = () => {
-        dispatch(logout());
-    };
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
 
   return (
-    <div>
-      <span>
-        {name}
-      </span>
-
+    <div className={styles.list}>
+      <div className={styles.personalInf}>
+        <span className={styles.avatar}>{/* {avatar} */}U</span>
+        <span className={styles.name}>{/* {name} */}User Name</span>
+      </div>
+      {/* <svg width="16" height="16">
+        <use href={`${logoutIcon}#logoutIcon`}></use>
+      </svg> */}
       <Button
-          title="Logout"
-          handleClick={() => handleLogout}
-          type="button"/>
-
+        title="Выйти"
+        className={styles.buttonLogout}
+        handleClick={() => handleLogout}
+        type="button"
+      />
     </div>
   );
 }
