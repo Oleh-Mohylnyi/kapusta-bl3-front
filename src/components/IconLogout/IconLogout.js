ecb103a6e32c4be6708c0b2e6a00906829ff788d
Oleh-Mohylnyi <oleh.mohylnyi@gmail.com>
2022-02-06 17:14:51 +0200

    add

---
+++ src/components/IconLogout/IconLogout.js
@@ -0,0 +1,17 @@
+import styles from "./IconLogout.module.scss";
+import sprite from "../../images/sprite.svg";
+
+export default function IconLogout({ handleClick }) {
+  return (
+    <>
+      <svg
+        width="16"
+        height="16"
+        className={styles.icon}
+        // onClick={handleClick()}
+      >
+        <use href={`${sprite}#logout-icon`}></use>
+      </svg>
+    </>
+  );
+}
--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,28 +1,33 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../redux/auth/operations'
-import { getUserName } from '../../redux/auth/selectors'
-import Button from '../Button/Button'
-
+// import { useDispatch, useSelector } from "react-redux";
+// import { logout } from "../../redux/auth/operations";
+// import { getUserName } from "../../redux/auth/selectors";
+import Button from "../ButtonLogout/ButtonLogout";
+import IconLogout from "../IconLogout/IconLogout";
+import styles from "./userMenu.module.scss";
 
 export default function UserMenu() {
-  const name = useSelector(getUserName);
-    const dispatch = useDispatch();
-    
-    const handleLogout = () => {
-        dispatch(logout());
-    };
+  // const name = useSelector(getUserName);
+  // const dispatch = useDispatch();
+
+  // const avatar = useSelector(getUserAvatar);
+  // const dispatch = useDispatch();
+
+  // const handleLogout = () => {
+  //   dispatch(logout());
+  // };
 
   return (
-    <div>
-      <span>
-        {name}
-      </span>
-
+    <div className={styles.item}>
+      <div className={styles.personalInf}>
+        <span className={styles.avatar}>{/*avatar*/}U</span>
+        <span className={styles.name}>{/*name*/}User Name</span>
+        <span className={styles.vector}>|</span>
+      </div>
+      <IconLogout></IconLogout>
       <Button
-          title="Logout"
-          handleClick={() => handleLogout}
-          type="button"/>
-
+        title="Выйти"
+        /*handleClick={() => handleLogout}*/ type="button"
+      />
     </div>
   );
 }
