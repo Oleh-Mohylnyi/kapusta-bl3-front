1ce2b12b67c01536529d610062dc1f41701ee20e
Oleh-Mohylnyi <79149758+Oleh-Mohylnyi@users.noreply.github.com>
2022-02-07 15:43:01 +0200

    Update UserMenu.js

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,23 +1,23 @@
-// import { useDispatch, useSelector } from 'react-redux'
-// import { logout } from '../../redux/auth/operations'
-// import { getUserEmail } from '../../redux/auth/selectors'
-// import Button from '../Button/Button'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../../redux/auth/operations'
+import { getUserEmail } from '../../redux/auth/selectors'
+import Button from '../Button/Button'
 
 export default function UserMenu() {
 
-  // const name = useSelector(getUserName);
-  // const name = useSelector(getUserEmail)
-  // const dispatch = useDispatch()
+  const name = useSelector(getUserName);
+  const name = useSelector(getUserEmail)
+  const dispatch = useDispatch()
 
-  // const handleLogout = () => {
-  //   dispatch(logout())
-  // }
+  const handleLogout = () => {
+    dispatch(logout())
+  }
 
   return (
     <div>
-      {/* <span>{name}</span>
+      {<span>{name}</span>
 
-      <Button title="Logout" handleClick={() => handleLogout} type="button" /> */}
+      <Button title="Logout" handleClick={() => handleLogout} type="button" />}
     </div>
   )
 }
