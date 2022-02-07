dc9e6eed234edb60d7e6b5753362a9d69307ad4e
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2022-02-07 22:49:55 +0200

    Update StatisticsView.js

--- src/views/StatisticsView/StatisticsView.js
+++ src/views/StatisticsView/StatisticsView.js
@@ -1,14 +1,28 @@
-import { Outlet } from "react-router-dom";
 import DiagramMobile from "../../components/StatisticDiagram/DiagramMobile";
+import DiagramTablet from "../../components/StatisticDiagram/DiagramTablet";
 import DiagramContainer from "../../components/StatisticDiagram/DiagramContainer";
+import Report from "../../components/Report/Report/Report";
+import PageContainer from "../../components/PageContainer";
+import TotalReport from "../../components/TotalReport/TotalReport";
+import BackgroundImages from "../../components/BackgroundLayouts/PrivatePageBg";
+import { Outlet } from "react-router-dom";
 
 export default function StatisticsView() {
   let width = window.innerWidth;
   return (
     <>
-      <Outlet />
-
-      {width < 768 ? <DiagramMobile /> : <DiagramContainer />}
+      <PageContainer>
+        <TotalReport />
+        <Outlet />
+        {width < 768 ? (
+          <DiagramMobile />
+        ) : (
+          <DiagramContainer>
+            <DiagramTablet />
+          </DiagramContainer>
+        )}
+        <BackgroundImages />
+      </PageContainer>
     </>
   );
 }
