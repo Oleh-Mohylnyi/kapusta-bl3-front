40e7c8a2d060bb07d100415be5cdc5db1c85d65b
Yudina Tamila <taaamilaaa1988@gmail.com>
2022-02-07 11:51:43 +0200

    update

--- src/views/StatisticsView/StatisticsView.js
+++ src/views/StatisticsView/StatisticsView.js
@@ -1,4 +1,6 @@
+import PageContainer from "../../components/PageContainer"
 import DiagramMobile from "../../components/StatisticDiagram/DiagramMobile";
+import DiagramTablet from "../../components/StatisticDiagram/DiagramTablet"
 import DiagramContainer from "../../components/StatisticDiagram/DiagramContainer";
 import Report from "../../components/Report/Report/Report";
 
@@ -6,8 +8,13 @@ export default function StatisticsView() {
   let width = window.innerWidth;
   return (
     <>
+      <PageContainer>
       <Report />
-      {width < 768 ? <DiagramMobile /> : <DiagramContainer />}
+        {width < 768 ? <DiagramMobile /> : <DiagramContainer>
+        <DiagramTablet/>
+        </DiagramContainer>}
+      </PageContainer>
+
     </>
   );
 }
