8a7cbf7d1039db0fcb3813eb74bada41d401dc05
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2022-02-07 23:02:22 +0200

    update

---
+++ src/components/TotalReport/TotalReport.js
@@ -0,0 +1,24 @@
+import s from "./TotalReport.module.css";
+
+const TotalReport = () => {
+  //из редакса берем общие суммы доходов и расходов
+  return (
+    <div className={s.container}>
+      <ul className={s.list}>
+        <li className={s.item}>
+          <div className={s.item__content}>
+            <h3 className={s.title}>Расходы:</h3>
+            <span className={s.costs}>-18000.00 грн</span>
+          </div>
+        </li>
+        <li className={s.item}>
+          <div className={s.item__content}>
+            <h3 className={s.title}>Доходы:</h3>
+            <span className={s.incomes}>+50000.00 грн</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default TotalReport;
