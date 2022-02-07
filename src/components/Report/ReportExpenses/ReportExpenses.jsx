9a0b90fc8ab5b3f5a94ff73064935c8fb093041f
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2022-02-07 22:39:51 +0200

    update

--- src/components/Report/ReportExpenses/ReportExpenses.jsx
+++ src/components/Report/ReportExpenses/ReportExpenses.jsx
@@ -6,27 +6,26 @@ import s from "./ReportExpenses.scss";
 
 export default function ReportExpenses() {
   const typesExpenses = type;
-  return (
-    <>
-      <div className={s.section}>
-        <div className={s.tab}>
-          <NavLink
-            to="/statistics/income"
-            className={(navData) => (navData.isActive ? s.activeLink : s.link)}
-          >
-            <ReportIcon name="arrow-left" size="10" />
-          </NavLink>
-          <h2 className={s.title}>Расходы</h2>
-          <NavLink
-            to="/statistics/income"
-            className={(navData) => (navData.isActive ? s.activeLink : s.link)}
-          >
-            <ReportIcon name="arrow-right" size="10" />
-          </NavLink>
-        </div>
 
-        {typesExpenses.length > 0 && <ReportList types={typesExpenses} />}
+  return (
+    <div className={s.section}>
+      <div className={s.tab}>
+        <NavLink
+          to="/statistics/income"
+          className={(navData) => (navData.isActive ? s.activeLink : s.link)}
+        >
+          <ReportIcon name="arrow-left" size="10" />
+        </NavLink>
+        <h2 className={s.title}>Расходы</h2>
+        <NavLink
+          to="/statistics/income"
+          className={(navData) => (navData.isActive ? s.activeLink : s.link)}
+        >
+          <ReportIcon name="arrow-right" size="10" />
+        </NavLink>
       </div>
-    </>
+
+      {typesExpenses.length > 0 && <ReportList types={typesExpenses} />}
+    </div>
   );
 }
--- src/components/Report/ReportIncome/ReportIncome.jsx
+++ src/components/Report/ReportIncome/ReportIncome.jsx
@@ -7,26 +7,24 @@ import s from "./ReportIncome.scss";
 export default function ReportIncome() {
   const typesIncome = type;
   return (
-    <>
-      <div className={s.section}>
-        <div className={s.tab}>
-          <NavLink
-            to="/statistics/expenses"
-            className={(navData) => (navData.isActive ? s.activeLink : s.link)}
-          >
-            <ReportIcon name="arrow-left" size="10" />
-          </NavLink>
-          <h2 className={s.title}>Доходы</h2>
-          <NavLink
-            to="/statistics/expenses"
-            className={(navData) => (navData.isActive ? s.activeLink : s.link)}
-          >
-            <ReportIcon name="arrow-right" size="10" />
-          </NavLink>
-        </div>
-
-        {typesIncome.length > 0 && <ReportList types={typesIncome} />}
+    <div className={s.section}>
+      <div className={s.tab}>
+        <NavLink
+          to="/statistics/expenses"
+          className={(navData) => (navData.isActive ? s.activeLink : s.link)}
+        >
+          <ReportIcon name="arrow-left" size="10" />
+        </NavLink>
+        <h2 className={s.title}>Доходы</h2>
+        <NavLink
+          to="/statistics/expenses"
+          className={(navData) => (navData.isActive ? s.activeLink : s.link)}
+        >
+          <ReportIcon name="arrow-right" size="10" />
+        </NavLink>
       </div>
-    </>
+
+      {typesIncome.length > 0 && <ReportList types={typesIncome} />}
+    </div>
   );
 }
