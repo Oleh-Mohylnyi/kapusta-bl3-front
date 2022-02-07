ac8993859949d92055501f39db744feb400a3650
Vitaliy <chichygav@gmail.com>
2022-02-07 18:40:32 +0200

    aad Summary

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -1,11 +1,12 @@
-import PageContainer from '../../components/PageContainer'
-import BackgroundImages from '../../components/BackgroundLayouts/PrivatePageBg'
+import PageContainer from "../../components/PageContainer";
+import BackgroundImages from "../../components/BackgroundLayouts/PrivatePageBg";
+import Summary from "./../../components/Summary/Summary";
 
 export default function MainView() {
-
-    return (
-        <PageContainer>
-            <BackgroundImages />
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      <BackgroundImages />
+      <Summary />
+    </PageContainer>
+  );
 }
