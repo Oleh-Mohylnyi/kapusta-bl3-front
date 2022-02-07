b837d2048ce23bf296b2d98d7ceab2c33f5a78b4
Tatiana-goit <tanchou74@gmail.com>
2022-02-07 16:20:41 +0200

    5 вариант...

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,33 +1,28 @@
-// import { useDispatch, useSelector } from "react-redux";
-// import { logout } from "../../redux/auth/operations";
-// import { getUserName } from "../../redux/auth/selectors";
-import Button from "../ButtonLogout/ButtonLogout";
-import IconLogout from "../IconLogout/IconLogout";
-import styles from "./userMenu.module.scss";
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../redux/auth/operations'
+import { getUserEmail } from '../../redux/auth/selectors'
+import Button from '../Button/Button'
+
 
 export default function UserMenu() {
-  // const name = useSelector(getUserName);
-  // const dispatch = useDispatch();
+  const name = useSelector(getUserEmail);
+    const dispatch = useDispatch();
 
-  // const avatar = useSelector(getUserAvatar);
-  // const dispatch = useDispatch();
-
-  // const handleLogout = () => {
-  //   dispatch(logout());
-  // };
+    const handleLogout = () => {
+        dispatch(logout());
+    };
 
   return (
-    <div className={styles.item}>
-      <div className={styles.personalInf}>
-        <span className={styles.avatar}>{/*avatar*/}U</span>
-        <span className={styles.name}>{/*name*/}User Name</span>
-        <span className={styles.vector}>|</span>
-      </div>
-      <IconLogout></IconLogout>
+    <div>
+      <span>
+        {name}
+      </span>
+
       <Button
-        title="Выйти"
-        /*handleClick={() => handleLogout}*/ type="button"
-      />
+          title="Logout"
+          handleClick={() => handleLogout}
+          type="button"/>
+
     </div>
   );
 }
