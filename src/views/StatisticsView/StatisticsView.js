0f06c04dbf9a0d82fe050cd631ea3d36064b8ceb
Yudina Tamila <taaamilaaa1988@gmail.com>
2022-02-07 11:54:06 +0200

    Update StatisticsView.js

--- src/views/StatisticsView/StatisticsView.js
+++ src/views/StatisticsView/StatisticsView.js
@@ -1,20 +1,23 @@
-import PageContainer from "../../components/PageContainer"
 import DiagramMobile from "../../components/StatisticDiagram/DiagramMobile";
-import DiagramTablet from "../../components/StatisticDiagram/DiagramTablet"
+import DiagramTablet from "../../components/StatisticDiagram/DiagramTablet";
 import DiagramContainer from "../../components/StatisticDiagram/DiagramContainer";
 import Report from "../../components/Report/Report/Report";
+import PageContainer from "../../components/PageContainer/PageContainer";
 
 export default function StatisticsView() {
   let width = window.innerWidth;
   return (
     <>
       <PageContainer>
-      <Report />
-        {width < 768 ? <DiagramMobile /> : <DiagramContainer>
-        <DiagramTablet/>
-        </DiagramContainer>}
+        <Report />
+        {width < 768 ? (
+          <DiagramMobile />
+        ) : (
+          <DiagramContainer>
+            <DiagramTablet />
+          </DiagramContainer>
+        )}
       </PageContainer>
-
     </>
   );
 }
