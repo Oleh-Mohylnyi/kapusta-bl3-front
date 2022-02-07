dacf03be2e94bc36b7e16f7b266bdde9a491e59c
Vitaliy <chichygav@gmail.com>
2022-02-07 20:22:59 +0200

    add TableData

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -1,11 +1,14 @@
-import PageContainer from '../../components/PageContainer'
-import BackgroundImages from '../../components/BackgroundLayouts/PrivatePageBg'
+import PageContainer from "../../components/PageContainer";
+import BackgroundImages from "../../components/BackgroundLayouts/PrivatePageBg";
+import SectionTransactions from "../../components/SectionTransactions";
+import TableData from "./../../components/SectionTransactions/TableData/TableData";
 
 export default function MainView() {
-
-    return (
-        <PageContainer>
-            <BackgroundImages />
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      <BackgroundImages />
+      <SectionTransactions />
+      <TableData />
+    </PageContainer>
+  );
 }
