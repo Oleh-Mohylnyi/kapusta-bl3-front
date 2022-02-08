3d54e79a91072fd4c7dbeee39c99adfcfe6050c8
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2022-02-08 07:09:05 +0200

    add routes for statistics page

--- src/App/App.js
+++ src/App/App.js
@@ -5,21 +5,34 @@ import { Route, Routes } from "react-router-dom";
 import { PrivateRoute } from "../routes/PrivateRoute";
 import { PublicRoute } from "../routes/PublicRoute";
 
-import { useDispatch, useSelector } from 'react-redux';
-import {currentUser} from '../redux/auth/operations'
-import { useEffect } from 'react';
-import { lazy, Suspense } from 'react';
-import { getIsAuth } from '../redux/auth/selectors';
-import { getToken } from '../redux/auth/selectors';
-import { getIsFetchingCurrent } from '../redux/auth/selectors';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Navigation } from '../components/Navigation/Navigation';
-const Header = lazy(() => import('../components/Header/Header' /* webpackChunkName: "header-view" */));
-const HomeView = lazy(() => import('../views/HomeView/HomeView' /* webpackChunkName: "home-view" */));
+import { useDispatch, useSelector } from "react-redux";
+import { currentUser } from "../redux/auth/operations";
+import { useEffect } from "react";
+import { lazy, Suspense } from "react";
+import { getIsAuth } from "../redux/auth/selectors";
+import { getToken } from "../redux/auth/selectors";
+import { getIsFetchingCurrent } from "../redux/auth/selectors";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Navigation } from "../components/Navigation/Navigation";
+import ReportIncome from "../components/Report/ReportIncome/ReportIncome";
+import ReportExpenses from "../components/Report/ReportExpenses/ReportExpenses";
 
-const MainView = lazy(() => import('../views/MainView/MainView' /* webpackChunkName: "contacts-view" */));
-const StatisticsView = lazy(() => import('../views/StatisticsView/StatisticsView' /* webpackChunkName: "login-view" */));
+const Header = lazy(() =>
+  import("../components/Header/Header" /* webpackChunkName: "header-view" */)
+);
+const HomeView = lazy(() =>
+  import("../views/HomeView/HomeView" /* webpackChunkName: "home-view" */)
+);
+
+const MainView = lazy(() =>
+  import("../views/MainView/MainView" /* webpackChunkName: "contacts-view" */)
+);
+const StatisticsView = lazy(() =>
+  import(
+    "../views/StatisticsView/StatisticsView" /* webpackChunkName: "login-view" */
+  )
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -31,28 +44,42 @@ function App() {
     // eslint-disable-next-line
   }, [dispatch]);
 
-  
-   
   return (
     <div className="app">
-      <Navigation/>
-        <Suspense fallback={ <Loader type="ThreeDots" color="#ff751d" height={80} width={80} />}>
-            <Header isAuth={isAuth}/>
-        {(isFetchingCurrent)
-          ? <Loader type="ThreeDots" color="#ff751d" height={80} width={80} />
-          : <Routes>
-              <Route path="/" element={<PublicRoute isAuth={isAuth} component={HomeView} />} />
-              <Route path="/main" element={<PrivateRoute isAuth={isAuth} component={MainView} />} />
-              <Route path="/statistics" element={<PrivateRoute isAuth={isAuth} component={StatisticsView} />} />
-          </Routes>}
-
+      <Navigation />
+      <Suspense
+        fallback={
+          <Loader type="ThreeDots" color="#ff751d" height={80} width={80} />
+        }
+      >
+        <Header isAuth={isAuth} />
+        {isFetchingCurrent ? (
+          <Loader type="ThreeDots" color="#ff751d" height={80} width={80} />
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={<PublicRoute isAuth={isAuth} component={HomeView} />}
+            />
+            <Route
+              path="/main"
+              element={<PrivateRoute isAuth={isAuth} component={MainView} />}
+            />
+            <Route
+              path="statistics"
+              element={
+                <PrivateRoute isAuth={isAuth} component={StatisticsView} />
+              }
+            >
+              <Route path="income" element={<ReportIncome />} />
+              <Route path="expenses" element={<ReportExpenses />} />
+            </Route>
+          </Routes>
+        )}
       </Suspense>
       <ToastContainer autoClose={3000} />
-
-      
-
-      </div>
-    );
+    </div>
+  );
 }
 
 export default App;
--- src/views/StatisticsView/StatisticsView.js
+++ src/views/StatisticsView/StatisticsView.js
@@ -1,20 +1,18 @@
+import { Outlet } from "react-router-dom";
 import DiagramMobile from "../../components/StatisticDiagram/DiagramMobile";
 import DiagramTablet from "../../components/StatisticDiagram/DiagramTablet";
 import DiagramContainer from "../../components/StatisticDiagram/DiagramContainer";
-import Report from "../../components/Report/Report/Report";
-
-import PageContainer from '../../components/PageContainer'
+import PageContainer from "../../components/PageContainer";
 import TotalReport from "../../components/TotalReport/TotalReport";
-import BackgroundImages from '../../components/BackgroundLayouts/PrivatePageBg'
+import BackgroundImages from "../../components/BackgroundLayouts/PrivatePageBg";
 
 export default function StatisticsView() {
   let width = window.innerWidth;
   return (
     <>
       <PageContainer>
-
-        <TotalReport/>
-        <Report />
+        <TotalReport />
+        <Outlet />
         {width < 768 ? (
           <DiagramMobile />
         ) : (
