cf134aed09c99719e804e8366e831b65890d384c
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2022-02-08 19:10:58 +0200

    balance-navigation
    додала навигацию, она еще не закончена, нужно правильно отстилизировать, добавить логику,  изменила модалку, вроде передаю текст через пропсы

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -1,10 +1,12 @@
 import BackgroundImages from "../../components/BackgroundLayouts/PrivatePageBg";
 import ComposeSection from "../../components/SectionTransactions/ComposeSection";
+import BalanceNavigation from "../../components/Navigation/BalanceNavigation/BalanceNavigation";
 
 export default function MainView() {
   return (
     <>
       <BackgroundImages />
+      <BalanceNavigation/>
       <ComposeSection />
     </>
   );
