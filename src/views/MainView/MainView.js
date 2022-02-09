18f438c3b11145f6c0c1525252048c31371f1590
ILLIA Kahanov <kahanov.illia@gmail.com>
2022-02-09 06:28:18 +0200

    delete some imports in MainPage.js

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -2,7 +2,6 @@ import BackgroundImages from "../../components/BackgroundLayouts/PrivatePageBg";
 // import SectionTransactions from "../../components/SectionTransactions";
 // import TableData from "./../../components/SectionTransactions/TableData/TableData";
 // import Summary from "./../../components/Summary/Summary";
-import TransactionForm from "../../components/TransactionForm";
 import ComposeSection from "../../components/SectionTransactions/ComposeSection";
 import BalanceNavigation from "../../components/Navigation/BalanceNavigation/BalanceNavigation";
 import TransactionsNav from "../../components/SectionTransactions/TransactionsNav";
