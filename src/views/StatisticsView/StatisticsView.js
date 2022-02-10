d7e24820b2154087f7e046d63d4e7508bb75294d
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2022-02-10 21:14:48 +0200

    +

--- src/views/StatisticsView/StatisticsView.js
+++ src/views/StatisticsView/StatisticsView.js
@@ -3,7 +3,13 @@ import Diagram from "../../components/StatisticDiagram/Diagram";
 import DiagramContainer from "../../components/StatisticDiagram/DiagramContainer";
 import TotalReport from "../../components/TotalReport/TotalReport";
 import BackgroundImages from "../../components/BackgroundLayouts/PrivatePageBg";
-import Report from "../../components/Report/Report/Report";
+<<<<<<< Updated upstream
+
+=======
+import Report from "../../components/Report/Report";
+import Balance from "../../components/Balance";
+import BackToMain from '../../components/BackToMain';
+>>>>>>> Stashed changes
 import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 export default function StatisticsView() {
