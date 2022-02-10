9ac78c97ff9b626c0144f7c51fbe317de6f66aa2
Vitaliy <chichygav@gmail.com>
2022-02-11 00:41:02 +0200

    add

--- src/components/SectionTransactions/ComposeSection/ComposeSection.js
+++ src/components/SectionTransactions/ComposeSection/ComposeSection.js
@@ -1,14 +1,34 @@
 import SectionTransactions from "../../SectionTransactions/SectionTransactions";
 import Summary from "../../Summary/Summary";
-import TableData from "../../SectionTransactions/TableData/TableData";
-import TransactionForm from "../../TransactionForm";
+import TransactionForm from "../../TransactionForm/TransactionForm";
+import TableData from "../ComposeSection/ComposeSection.js";
+// import useWResize from "../../../hooks/useWindowDimensions";
 import s from "./ComposeSection.module.scss";
+// const ExpensePage = () => {
+//   const viewPort = useWResize();
+//   return (
+//     <div>
+//       <SectionTransactions>
+//         {viewPort.width >= 768 && <TransactionForm />}
+//         <div className={s.wrapperTables}>
+//           <TableData
+//           // isExpense={true}
+//           // dataTransactions={filterForDate() || []}
+//           // onChangeDel={onDelExpenseDataApi}
+//           />
+//           <Summary />
+//         </div>
+//       </SectionTransactions>
+//     </div>
+//   );
+// };
+// export default ExpensePage;
 
 const ExpensePage = () => {
   return (
     <div>
       <SectionTransactions>
-        <TransactionForm/>
+        <TransactionForm />
         <div className={s.wrapperTables}>
           <TableData isExpense={true} />
           <Summary />
@@ -19,3 +39,21 @@ const ExpensePage = () => {
 };
 
 export default ExpensePage;
+
+// return (
+//     <div>
+//       <SectionTransactions>
+//         {viewPort.width >= 768 && <AddDataForm />}
+//         <div className={s.wrapperTables}>
+//           <TableData
+//             isExpense={true}
+//             dataTransactions={filterForDate() || []}
+//             onChangeDel={onDelExpenseDataApi}
+//           />
+//           <Summary monthsStats={expenseMonthsStats} />
+//         </div>
+//       </SectionTransactions>
+//     </div>
+//   );
+// };
+// export default ExpensePage;
