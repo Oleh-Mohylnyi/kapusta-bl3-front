6dd1a63c8b1af02d06835223fad103bc7436c47b
Vitaliy <chichygav@gmail.com>
2022-02-11 20:56:42 +0200

    add

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,37 +1,43 @@
 import React from "react";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 import s from "./summary.module.scss";
 
 const Summary = () => {
+  const viewPort = useWindowDimensions();
   return (
-    <div className={s.summary}>
-      <h4>Сводка</h4>
-      <ul className={s.summary_list}>
-        <li className={s.summary_item}>
-          <span>Ноябрь</span>
-          <span>1 000.00</span>
-        </li>
-        <li className={s.summary_item}>
-          <span>Октябрь</span>
-          <span>1 000.00</span>
-        </li>
-        <li className={s.summary_item}>
-          <span>Сентябрь</span>
-          <span>1 000.00</span>
-        </li>
-        <li className={s.summary_item}>
-          <span>Август</span>
-          <span>1 000.00</span>
-        </li>
-        <li className={s.summary_item}>
-          <span>Июль</span>
-          <span>1 000.00</span>
-        </li>
-        <li className={s.summary_item}>
-          <span>Июнь</span>
-          <span>1 000.00</span>
-        </li>
-      </ul>
-    </div>
+    <>
+      {viewPort.width > 768 && (
+        <div className={s.summary}>
+          <h4>Сводка</h4>
+          <ul className={s.summary_list}>
+            <li className={s.summary_item}>
+              <span>Ноябрь</span>
+              <span>1 000.00</span>
+            </li>
+            <li className={s.summary_item}>
+              <span>Октябрь</span>
+              <span>1 000.00</span>
+            </li>
+            <li className={s.summary_item}>
+              <span>Сентябрь</span>
+              <span>1 000.00</span>
+            </li>
+            <li className={s.summary_item}>
+              <span>Август</span>
+              <span>1 000.00</span>
+            </li>
+            <li className={s.summary_item}>
+              <span>Июль</span>
+              <span>1 000.00</span>
+            </li>
+            <li className={s.summary_item}>
+              <span>Июнь</span>
+              <span>1 000.00</span>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 export default Summary;
