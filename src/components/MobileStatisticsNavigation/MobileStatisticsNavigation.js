f4aa68e73a27cb9a855b32e2f574543a6a2ce116
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2022-02-12 19:47:48 +0200

    netlif

--- src/components/MobileStatisticsNavigation/MobileStatisticsNavigation.js
+++ src/components/MobileStatisticsNavigation/MobileStatisticsNavigation.js
@@ -2,7 +2,7 @@ import BalanceTitle from "../../components/BalanceNavigationSection/BalanceTitle
 import Balance from "../../components/BalanceNavigationSection/Balance";
 import BackToMain from "../../components/BalanceNavigationSection/BackToMain";
 import Period from '../../components/Period';
-import s from "./MobileStatisticsNavigation.module.css";
+
 
 export default function MobileStatisticsNavigation () {
 return (
