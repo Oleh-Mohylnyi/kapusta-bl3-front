c63f74e45bdbcc4d9ef8c51bd9606eeacd96c387
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2022-02-13 15:26:59 +0200

    period
    перенесла период в статистику

--- src/components/MobileStatisticsNavigation/MobileStatisticsNavigation.js
+++ src/components/MobileStatisticsNavigation/MobileStatisticsNavigation.js
@@ -3,10 +3,13 @@ import Balance from "../../components/BalanceNavigationSection/Balance";
 import BackToMain from "../../components/BalanceNavigationSection/BackToMain";
 import Period from '../../components/Period';
 
-export default function MobileStatisticsNavigation () {
+export default function MobileStatisticsNavigation ({ handlePreviousPeriod,
+    periodMonth,
+    periodYear,
+    handleNextPeriod}) {
 return (
     <><BackToMain />
-    <Period />
+    <Period handlePreviousPeriod={handlePreviousPeriod} periodMonth={periodMonth} periodYear={periodYear} handleNextPeriod={handleNextPeriod}/>
     <BalanceTitle />
     <Balance /></>
 )
