34442e56737538dcac24d694edf8255a0fe3cee6
Andrey Volchenskiy <79094272+andvolch@users.noreply.github.com>
2022-02-16 18:50:20 +0200

    Update MainView.js

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -4,30 +4,30 @@ import BackgroundImages from "../../components/BackgroundLayouts/PrivatePageBg";
 // import Summary from "./../../components/Summary/Summary";
 import BalanceNavigation from "../../components/BalanceNavigationSection/BalanceNavigation";
 import { useEffect } from "react";
-// import TransactionsNav from "../../components/SectionTransactions/TransactionsNav";
+import TransactionsNav from "../../components/SectionTransactions/TransactionsNav";
 import { fetchTransactionsThunk } from "../../redux/transactions/transactionsThunks";
 import { useDispatch } from "react-redux";
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function MainView() {
-  // const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchTransactionsThunk());
   }, [dispatch]);
 
-  // const switcher = (val) => {
-  //   //switch from expenses to income
-  //   setToggle(val);
-  // };
+  const switcher = (val) => {
+    //switch from expenses to income
+    setToggle(toggle);
+  };
   return (
     <>
       {/* <SectionTransactions /> */} {/* УДАЛИТЬ?? */}
       {/*<TableData />*/} {/* УДАЛИТЬ?? */}
       {/* <Summary /> */} {/* УДАЛИТЬ?? */}
       <BalanceNavigation />
-      {/* <TransactionsNav onClick={switcher} /> */}
+      <TransactionsNav onClick={switcher} />
       {/* <SectionTransactions /> */}
       <BackgroundImages />
     </>
