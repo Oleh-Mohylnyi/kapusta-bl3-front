2112c1cc3e88b5433d7467b52f6e63f7b51ef42a
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2022-02-16 18:42:50 +0200

    updates

--- src/components/TabletDesktopStatisticsNavigation/TabletDesktopStatisticsNavigation.js
+++ src/components/TabletDesktopStatisticsNavigation/TabletDesktopStatisticsNavigation.js
@@ -1,9 +1,9 @@
-import Button from "../../components/BalanceNavigationSection/Button";
+// import Button from "../../components/BalanceNavigationSection/Button";
 import BalanceTitle from "../../components/BalanceNavigationSection/BalanceTitle";
 import Balance from "../../components/BalanceNavigationSection/Balance";
 import BackToMain from "../../components/BalanceNavigationSection/BackToMain";
 import Period from '../../components/Period';
-import useWindowDimensions from "../../hooks/useWindowDimensions";
+// import useWindowDimensions from "../../hooks/useWindowDimensions";
 import s from "./TabletDesktopStatisticsNavigation.module.css";
 
 export default function TabletDesktopStatisticsNavigation ({handlePreviousPeriod,
@@ -12,8 +12,8 @@ export default function TabletDesktopStatisticsNavigation ({handlePreviousPeriod
     handleNextPeriod,
     onGoBackClick, 
     setNotVisible}) {
-    const windowSize = useWindowDimensions();
-    const desktopView = windowSize.width >= 1280;
+    // const windowSize = useWindowDimensions();
+    // const desktopView = windowSize.width >= 1280;
    
     return (
         <div className={s.desktopBalance}>
